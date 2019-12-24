59a1b1a73f9897adf406ced7e969649456e4b0fe
liii18 <1262263306@qq.com>
2019-12-24 16:38:17 +0800

    update tdt key and change varieble names/format

--- app/config/setting.py
+++ app/config/setting.py
@@ -4,8 +4,9 @@ TOKEN_EXPIRATION = 30 * 24 * 3600
 BUILDINGS_TABLE = "BUIA"
 
 # tianditu and google map remote sensing wmts url
-URL_TDT = '''https://t1.tianditu.gov.cn/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=8971e4c7b3640d506c2dc111221af6a0'''
+URL_TDT = '''https://t1.tianditu.gov.cn/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=4830425f5d789b48b967b1062deb8c71'''
 URL_GOOGLE = '''http://ditu.google.cn/maps/vt/lyrs=s&x={x}&y={y}&z={z}'''
+# URL_TDT = '''http://yingxiang2019.geo-compass.com/api/wmts?layer=s%3Azjw&style=time%3D1576222648262&tilematrixset=w&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}&threshold=100'''
 
 # config.toml and checkpoint.pth files path
 ROBOSAT_DATA_PATH = "data"
