2040ba9b54c50079ce74c3b5229f9dd3b1e8859a
liii18 <1262263306@qq.com>
2020-01-14 09:35:28 +0800

    add RSPreturn_predict

---
+++ robosat_pink/geoc/RSPreturn_predict.py
@@ -0,0 +1,92 @@
+import os
+from robosat_pink.tools import cover, download, rasterize, predict, vectorize
+from robosat.tools import feature, merge
+import time
+import shutil
+import json
+import multiprocessing
+
+from robosat_pink.geoc import config as CONFIG, params, utils
+
+multiprocessing.set_start_method('spawn', True)
+
+
+def main(extent, dataPath, dsPath, map="google", auto_delete=False):
+    # training or predict checkpoint.pth number
+    # pthNum = utils.getLastPth(dataPath)
+    # if pthNum == 0:
+    #     return 'No model was found in directory for prediction'
+
+    params_cover = params.Cover(
+        bbox=extent,
+        zoom=18, out=[dsPath + "/cover"])
+    cover.main(params_cover)
+
+    # params_download = params.Download(
+    #     type="XYZ",
+    #     url=CONFIG.WMTS_HOST+"/{z}/{x}/{y}?type="+map,
+    #     # url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    #     # url='https://b.tiles.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXFhYTA2bTMyeW44ZG0ybXBkMHkifQ.gUGbDOPUN1v1fTs5SeOR4A'
+    #     cover=dsPath + "/cover",
+    #     out=dsPath + "/images",
+    #     timeout=20)
+    # download.main(params_download)
+
+    # pthPath = dataPath + "/model/checkpoint-" + \
+    #     str(pthNum).zfill(5)+".pth"
+
+    # params_predict = params.Predict(
+    #     dataset=dsPath,
+    #     checkpoint=pthPath,
+    #     config=dataPath+"/config.toml",
+    #     out=dsPath + "/masks"
+    # )
+    # predict.main(params_predict)
+
+    # params_vectorize = params.Vectorize(
+    #     masks=dsPath + "/masks",
+    #     type="Building",
+    #     config=dataPath+"/config.toml",
+    #     out=dsPath + "/vectors.json"
+    # )
+    # vectorize.main(params_vectorize)
+
+
+    # params_features = params.Features(
+    #     masks=dsPath + "/masks",
+    #     type="parking",
+    #     dataset=dataPath+"/config.toml",
+    #     out=dsPath + "/features.json"
+    # )
+    # feature.main(params_features)
+
+    # # 解析预测结果并返回
+    # jsonFile = open(dsPath + "/features.json", 'r')
+    # jsonObj = json.load(jsonFile)
+
+
+    # params_merge = params.Merge(
+    #     features=dsPath + "/features.json",
+    #     threshold=1,
+    #     out=dsPath + "/merged_features.json"
+    # )
+    # merge.main(params_merge)
+
+    params_subset_masks = params.Subset(
+        dir=dsPath+'/masks',
+        cover=dsPath+'/cover',
+        out=dsPath+'/masks'
+    )
+    subset.main(params_subset_masks)
+
+    # img to json
+    out = subset_features.json
+
+    # 解析预测结果并返回
+    jsonFile = open(dsPath + "/merged_features.json", 'r')
+    jsonObj = json.load(jsonFile)
+
+    # if auto_delete:
+    #     shutil.rmtree(dsPath)
+
+    return jsonObj
